cc4614c213f3f0bc390b1674004478e777bee6c8
UtechG17 <mgl.ngl.gtrrz.srz@gmail.com>
2024-07-16 01:50:47 -0400

    cuchito

--- Proyecto/vista/item_vista.php
+++ Proyecto/vista/item_vista.php
@@ -38,57 +38,57 @@ $idCategoria = isset($_GET['idCategoria']) ? $_GET['idCategoria'] : '0';
         </div>
     </div>
     <div class="contenedor-categoria px-6 pt-5">
-    <div id="tabla_div">
-    <div class="row">
-    <div class="col-sm-3">
-        <a href="#" class="modal_abrir btn btn-primary" onClick="agregarItem();">Agregar Item</a>
-    </div>
-<table id="tabla" class="table table-striped" style="width:100%">
-        <thead>
-                            <th>Estado</th>
-                            <!--<th>Categoria</th>-->
-                            <th>Item</th>
-                            <th>Cantidad</th>
-                            <th>Presupuesto</th>
-                            <th>Monto Gastado</th>
-                            <th>Acciones</th>
-                        </tr>
-                        <tbody>
-                            <?php
+        <div id="tabla_div">
+            <div class="row">
+                <div class="col-sm-3">
+                    <a href="#" class="modal_abrir btn btn-primary" onClick="agregarItem();">Agregar Item</a>
+                </div>
+                <table id="tabla" class="table table-striped" style="width:100%">
+                    <thead>
+                        <th>Estado</th>
+                        <!--<th>Categoria</th>-->
+                        <th>Item</th>
+                        <th>Cantidad</th>
+                        <th>Presupuesto</th>
+                        <th>Monto Gastado</th>
+                        <th>Acciones</th>
+                    </tr>
+                    <tbody>
+                        <?php
                             if (isset($data) && is_array($data)) {
-                            foreach ($data as $row) {
-                                $row['cantidad'] = $row['presupuesto'] = '';
-                                if( isset( $dataPresupuesto[ $row['id_item']] ) ){
-                                    $aux = $dataPresupuesto[ $row['id_item']];
-                                    $row['cantidad'] = $aux['cantidad'];
-                                    $row['presupuesto'] = $aux['monto_presupuesto'];
-                                }
-
-                                echo "<tr>";
-                                echo "<td>" . ($row['estado'] == 1 ? 'Activo' : 'Inactivo') . "</td>";
-                                echo "<td>" . $row['nombre'] . "</td>";
-                                echo "<td>" . $row['cantidad'] . "</td>";
-                                echo "<td>" . $row['presupuesto'] . "</td>";
-                                echo "<td>0.00</td>";
-                                echo "<td>
+                                foreach ($data as $row) {
+                                    $row['cantidad'] = $row['presupuesto'] = '';
+                                    if( isset( $dataPresupuesto[ $row['id_item']] ) ){
+                                        $aux = $dataPresupuesto[ $row['id_item']];
+                                        $row['cantidad'] = $aux['cantidad'];
+                                        $row['presupuesto'] = $aux['monto_presupuesto'];
+                                    }
+                                    echo "<tr>";
+                                    echo "<td>" . ($row['estado'] == 1 ? 'Activo' : 'Inactivo') . "</td>";
+                                    echo "<td>" . $row['nombre'] . "</td>";
+                                    echo "<td>" . $row['cantidad'] . "</td>";
+                                    echo "<td>" . $row['presupuesto'] . "</td>";
+                                    echo "<td>0.00</td>";
+                                    echo "<td>
                                         <a onClick='buscarItem(this)' class='btn-azul' data-id='".$row['id_item']."' data-nombre='".$row['nombre']."' data-estado='".$row['estado']."' data-cantidad='".$row['cantidad']."' data-presupuesto='".$row['presupuesto']."'><img src='../vista/img/editar.png' alt='editar'></a>
                                         <a href='?eliminarId=".$row['id_item']."&idProyecto=".$idProyecto."&idCategoria=".$idCategoria."' class='btn-rojo'><img src='../vista/img/eliminar.png' alt='eliminar'></a>
                                     </td>";
                                     echo "</tr>";
-                                }
-                            } else {
-                                echo "<tr><td colspan='4'>No hay datos disponibles.</td></tr>";
+                                    }
+                                } else {
+                                    echo "<tr><td colspan='4'>No hay datos disponibles.</td></tr>";
                             }
                         ?>
                     </tbody>
-        <tfoot>
-            <tr>
-            </tr>
-        </tfoot>
-    </table>
+                    <tfoot>
+                        <tr>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
     </div>
-    </div>
-    </div>
+
     <section id="modalItem" class="modal_section">
         <div class="modal__contenedor">
             <form id="itemForm" action="" method="POST">
@@ -131,7 +131,6 @@ $idCategoria = isset($_GET['idCategoria']) ? $_GET['idCategoria'] : '0';
     <script src="../vista/js/bootstrap.bundle.min.js"></script>
     <script src="../vista/js/dataTables.js"></script>
     <script src="../vista/js/dataTables.bootstrap5.js"></script>
-    
     <script src="../vista/js/tableScript.js"></script>
     <script src="../vista/js/modal_item.js"></script>
 
